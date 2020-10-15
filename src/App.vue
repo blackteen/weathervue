1f23e579feaf0038a89159dadf1ea0e779a2553d
unknown <darksnow91@ukr.net>
2020-10-15 20:02:27 +0300

    updated logic

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <template>
   <div id="app">
-  
+    <div class="weather-post" v-for="item in getWeather.weather" :key="item.id">
+      <p>Sity: {{ getWeather.name }}</p>
+      <p>
+        Weather:
+        {{ item.main }}
+      </p>
+      <p>
+        Description:
+        {{ item.description }}
+      </p>
 
+      <img :src="getImgUrl(item.icon)" />
+    </div>
   </div>
 </template>
 
 <script>
-
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-  name: 'App',
-  components: {
-   
-  }
-}
+  name: "App",
+  computed: mapGetters(["getWeather", "getLenght"]),
+  methods: {
+    ...mapActions(["fetcWeather"]),
+    getImgUrl(pic) {
+      return `http://openweathermap.org/img/wn/${pic}@2x.png`;
+    },
+  },
+  async mounted() {
+    this.fetcWeather();
+  },
+};
 </script>
 
 <style>
@@ -24,5 +42,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.weather-post {
+  border: 1px solid #111;
+  padding: 10px;
+  margin-bottom: 10px;
 }
 </style>
